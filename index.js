d6951f3599cebe6fad464ae7853ba8889ea701cb
alpinocode <alfinohasan629@gmail.com>
2024-01-03 05:10:46 +0700

    perubahan kelima

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const express =require("express");
 // memanggil express
 const app = express();
 
+// membuat routing 
+// app method(path, handler)
+// app.use("/", (req, res, next)=> {
+//     res.send('hello user')
+// })
+
+app.get("/", (req,res) => {
+    res.send('hello pojan')
+})
+
+app.post("/", (req,res) => {
+    res.send('hello alpin')
+})
+
+
 // membuat port 
 app.listen(4000,()=>{
     console.log('Server berhasil di running')
