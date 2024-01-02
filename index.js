e77104f412b0e4a533c57fcbbcb96fff2d6cb2b0
alpinocode <alfinohasan629@gmail.com>
2024-01-03 05:12:00 +0700

    perubahan keenam

--- index.js
+++ index.js
@@ -9,16 +9,21 @@ const app = express();
 //     res.send('hello user')
 // })
 
+// mendapatkan data
 app.get("/", (req,res) => {
     res.send('hello pojan')
 })
 
+// mengcreate data baru
 app.post("/", (req,res) => {
     res.send('hello alpin')
 })
+
+
 
 
 // membuat port 
 app.listen(4000,()=>{
     console.log('Server berhasil di running')
 })
+
