cdd453b0401394630e3f3ca338b6a46f119cf952
alpinocode <alfinohasan629@gmail.com>
2024-01-04 22:25:44 +0700

    perubahan kesepuluh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express =require("express");
+// connection to mysql
+const mysql = require('mysql2')
+
+
 // memanggil userRoutes
 const UserRoutes = require('./routes/Users')
 // memanggil middleware dari file logs.js
 const MiddlewareLogRequest = require("./middleware/logs")
+
+// mengkoneksikan mysql dengan pool
+const dbPool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'membuatrestapi'
+})
 
 // memanggil express
 const app = express();
@@ -18,6 +30,21 @@ app.use(express.json())
 
 // memanggil UserRoutes
 app.use("/users", UserRoutes)
+
+app.use('/', ( req, res ) => {
+    dbPool.execute('SELECT * FROM users', (err, rows) => {
+        if(err){
+            res.json({
+                message: 'Connection to failed'
+            })
+        }
+
+        res.json({
+            message: 'Connection success',
+            data: rows
+        })
+    })
+})
 
 
 // membuat port 
