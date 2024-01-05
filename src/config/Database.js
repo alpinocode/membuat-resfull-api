7628ad378e861d5319b7752a2b2a4f44a5d249ec
alpinocode <alfinohasan629@gmail.com>
2024-01-05 23:38:02 +0700

    perubahan pemanggilan melalui envirotment

--- src/config/Database.js
+++ src/config/Database.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2')
 
 // mengkoneksikan mysql dengan pool
 const dbPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'membuatrestapi'
+    // pemanggilan menggunakan envirotment
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 // karena dbPool bersifat asychronous maka menggunakan method promise()
