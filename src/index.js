7666985a68886b283ed17ac20285abcaf7722abf
alpinocode <alfinohasan629@gmail.com>
2024-01-05 23:33:50 +0700

    mengkonfigurasi envirotment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+// enviroment variabel
+require('dotenv').config();
+// memanggil enviroment
+const PORT = process.env.PORT || 5000;
+
 const express =require("express");
 
-
-
 // memanggil userRoutes
-const UserRoutes = require('./routes/Users')
+const UserRoutes = require('./routes/Users');
 // memanggil middleware dari file logs.js
-const MiddlewareLogRequest = require("./middleware/logs")
+const MiddlewareLogRequest = require("./middleware/logs");
 
 
 // memanggil express
@@ -15,19 +18,19 @@ const app = express();
 // app method(path, handler)
 
 // merouting dari struktur project dari middleware
-app.use( MiddlewareLogRequest )
+app.use( MiddlewareLogRequest );
 
 // menginzinkan request body berupa JSON
-app.use(express.json())
+app.use(express.json());
 
 // memanggil UserRoutes
-app.use("/users", UserRoutes)
+app.use("/users", UserRoutes);
 
 
 
 
 // membuat port 
-app.listen(4000,()=>{
-    console.log('Server berhasil di running')
+app.listen(PORT,()=>{
+    console.log(`Server berhasil di running${PORT}`)
 })
 
